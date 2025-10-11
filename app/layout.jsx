f2cf6eb41315aaa6ca08f0c1621f05082ed82932
Ömer Halis DEMİR <omerhd16@outlook.com>
2025-10-11 20:37:43 +0300

    Animasyon kaldırıldı.

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,14 +4,12 @@ import ScrollToTop from "../components/extra/ScrollToTop";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import ScrollToTopButton from "../components/extra/ScrollTopButton";
-import BackgroundAnimation from "../components/extra/BackgroundAnimation";
 import { LanguageProvider, useLanguage } from "./context/LanguageContext";
 
 function LayoutContent({ children }) {
  const { language, handleLanguageChange } = useLanguage();
  return (
   <div className="min-h-screen select-none transition-colors duration-300 ease-in-out bg-gray-900 relative">
-   <BackgroundAnimation />
    <div className="relative z-10">
     <Header language={language} onLanguageChange={handleLanguageChange} />
     <main>{children}</main>
