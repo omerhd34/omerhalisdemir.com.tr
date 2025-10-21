abf1da7ca22b87abbeed5cb96a67a7afb213e849
Ömer Halis DEMİR <omerhd16@outlook.com>
2025-10-21 15:38:40 +0300

    Güncellendi.

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.prepare().then(() => {
       process.exit(1);
     })
     .listen(port, hostname, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
+      console.log(`> Ready on http://localhost:${port}`);
     });
 });
