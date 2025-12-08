eccb2210c53496107dcb3cfd885f84f8dced0f02
Ömer Halis DEMİR <omerhd16@outlook.com>
2025-12-09 01:52:06 +0300

    Küçük güncelleme

--- app/api/experience/[lang]/route.js
+++ app/api/experience/[lang]/route.js
@@ -40,6 +40,7 @@ export async function GET(request, context) {
     technologies: technologies,
     achievements: achievements,
     icon: exp.icon,
+    displayOrder: exp.displayOrder,
    });
 
    return acc;
